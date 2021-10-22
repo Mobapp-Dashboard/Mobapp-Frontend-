37f8952364188900fe2a2f0ee30ff29d66267bbf
Saci <saciperere@riseup.net>
2021-10-22 09:53:08 -0300

    ajusta docker

--- environment/settings.py
+++ environment/settings.py
@@ -7,13 +7,12 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), os.getenv(
     "DASH_ENV_FILE", default =".env.development"))
 
-print(dotenv_path)
-print(os.environ.get("PORT"))
-
 load_dotenv(dotenv_path=dotenv_path, override=True)
+
 
 APP_HOST = os.environ.get("HOST")
 APP_PORT = int(os.environ.get("PORT"))
 APP_DEBUG = bool(os.environ.get("DEBUG"))
 DEV_TOOLS_PROPS_CHECK = bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
-BACKEND = str(os.environ.get("BACKEND"))
+BACKEND = str(os.environ.get("BACKEND")
+)
