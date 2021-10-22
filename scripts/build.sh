8704195c2521aef2e9e898fc73d613f0b9e50c81
Saci <saciperere@riseup.net>
2021-10-22 10:49:04 -0300

    tentando dar deploy =|

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,6 @@
 # Exit in case of error
 set -e
 
-
-VERSION=1.0.0 \
-TARGET=LIVE \
-DOMAIN=dash.klh.criptofesta.org \
-STACK_NAME=mobdash \
 docker-compose \
 -f docker-compose.yml \
 build
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,11 @@
 # Exit in case of error
 set -e
 
-VERSION=1.0.0 \
-TARGET=LIVE \
-DOMAIN=dash.klh.criptofesta.org \
-STACK_NAME=mobdash \
 docker-compose \
 -f docker-compose.yml \
 config > docker-stack.yml
 
 docker-auto-labels docker-stack.yml
 
-docker stack rm "${STACK_NAME}"
+# docker stack rm "${STACK_NAME}"
 docker stack deploy -c docker-stack.yml --with-registry-auth "${STACK_NAME?Variable not set}"
