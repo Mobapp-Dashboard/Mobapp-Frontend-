7a3c413c08eb26229235eb795f941e32fa2e49f3
Saci <saciperere@riseup.net>
2021-11-12 08:33:39 -0300

    recebe model_points

--- layout/__init__.py
+++ layout/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env ipython
 
+import time
+
 import dash_core_components as dcc
 import dash_html_components as html
 from app import app
@@ -8,35 +10,42 @@ from pages.anomalies_detection_region import adr
 from pages.eda import eda
 from pages.eval_model import eval_model
 from styles.style import TOP_TITLE_STYLE
-import time
 
-START_PAGE = "tab-eda"
+START_PAGE = "tab-pred-anom"
 
-layout = html.Div([
-    html.H1('Dashboard: Urban Mobility', style=TOP_TITLE_STYLE),
-    dcc.Loading(
-        id="loading-tabs",
-        type="default",
-        children=[
-            dcc.Tabs(id="front-tabs", value=START_PAGE, children=[
-                dcc.Tab(label='EDA', value='tab-eda'),
-                dcc.Tab(label='Model Evaluation', value='tab-eval-model'),
-                dcc.Tab(label='Anomalies Detection(Points/Region)', value='tab-pred-anon'),
-            ]),
-            html.Div(id='front-tabs-content')
-        ]
-    ),
-])
+layout = html.Div(
+    [
+        html.H1("Dashboard: Urban Mobility", style=TOP_TITLE_STYLE),
+        dcc.Loading(
+            id="loading-tabs",
+            type="default",
+            children=[
+                dcc.Tabs(
+                    id="front-tabs",
+                    value=START_PAGE,
+                    children=[
+                        dcc.Tab(label="EDA", value="tab-eda"),
+                        dcc.Tab(label="Model Evaluation", value="tab-eval-model"),
+                        dcc.Tab(
+                            label="Anomalies Detection(Points/Region)",
+                            value="tab-pred-anom",
+                        ),
+                    ],
+                ),
+                html.Div(id="front-tabs-content"),
+            ],
+        ),
+    ]
+)
 
 
-@app.callback(Output('front-tabs-content', 'children'),
-              Input('front-tabs', 'value'))
+@app.callback(Output("front-tabs-content", "children"), Input("front-tabs", "value"))
 def render_content(tab):
-    if tab == 'tab-eda':
+    if tab == "tab-eda":
         return eda.content
-    elif tab == 'tab-eval-model':
+    elif tab == "tab-eval-model":
         return eval_model.content
-    elif tab == "tab-pred-anon":
+    elif tab == "tab-pred-anom":
         return adr.page
 
 
--- pages/anomalies_detection_region/adr_data.py
+++ pages/anomalies_detection_region/adr_data.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env ipython
 
-from app import app
-from dash.dependencies import Input, Output, State
-
-import requests
 import pandas as pd
+import requests
 
+from app import app, cache
+from dash.dependencies import Input, Output, State
 from environment.settings import BACKEND
-from app import cache
 from utils.extract_data import get_traj
+
 
 def anom_by_model_route(model, route):
     url = f"http://{BACKEND}/api/v1/dublin_model/predictions/{model}/{route}"
@@ -19,19 +18,22 @@ def anom_by_model_route(model, route):
 def extract_anon_idx(df, traj):
     dft = df[df["trajectory_id"] == traj]
     anoms = dft["anon_predictions"].unique()[0]
-    anoms = anoms.replace("{","[").replace("}","]")
+    anoms = anoms.replace("{", "[").replace("}", "]")
     anoms = eval(anoms)
     return anoms
 
+
 def traj_by_rota_traj(rota, traj):
-    url = f"http://{BACKEND}/api/v1/dublin_model/meta_trajectory/?trajectory_id={traj}&routes={rota}"
+    url = f"http://{BACKEND}/api/v1/dublin_model/model_points/?trajectory_id={traj}&rota={rota}"
     df = get_traj(url, sort_by="index")
     return df
 
+
 def trajs_by_rota(rota):
-    url = f"http://{BACKEND}/api/v1/dublin_model/meta_trajectory/?routes={rota}"
+    url = f"http://{BACKEND}/api/v1/dublin_model/model_points/?rota={rota}"
     df = get_traj(url, sort_by="index")
     return df
+
 
 def get_eval(rota):
     models = ["transformer", "riobusdata", "gmvsae"]
